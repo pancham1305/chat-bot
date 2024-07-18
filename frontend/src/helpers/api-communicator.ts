d9d54a54a80167b9f21d35c3f961e92b182c887e
pancham1305 <pancham1305@gmail.com>
2024-07-18 23:46:42 +0530

    please god

--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -14,7 +14,11 @@ export const signupUser = async (
   email: string,
   password: string
 ) => {
-  const res = await axios.post("/user/signup", { name, email, password });
+  const res = await axios.post(
+    "/user/signup",
+    { name, email, password },
+    { withCredentials: true }
+  );
   if (res.status !== 201) {
     throw new Error("Signup failed");
   }
@@ -23,7 +27,7 @@ export const signupUser = async (
 };
 
 export const checkAuthStatus = async () => {
-  const res = await axios.get("/user/auth-status");
+  const res = await axios.get("/user/auth-status", { withCredentials: true });
   if (res.status !== 200) {
     throw new Error("Unauthorized");
   }
@@ -32,7 +36,11 @@ export const checkAuthStatus = async () => {
 };
 
 export const sendChatRequest = async (message: string) => {
-  const res = await axios.post("/chat/new", { message });
+  const res = await axios.post(
+    "/chat/new",
+    { message },
+    { withCredentials: true }
+  );
   if (res.status !== 200) {
     throw new Error("Unable to send message");
   }
@@ -41,7 +49,7 @@ export const sendChatRequest = async (message: string) => {
 };
 
 export const getUserChats = async () => {
-  const res = await axios.get("/chat/all-chats");
+  const res = await axios.get("/chat/all-chats", { withCredentials: true });
   if (res.status !== 200) {
     throw new Error("Unable to get chat");
   }
@@ -50,7 +58,7 @@ export const getUserChats = async () => {
 };
 
 export const deleteUserChats = async () => {
-  const res = await axios.delete("/chat/delete");
+  const res = await axios.delete("/chat/delete", { withCredentials: true });
   if (res.status !== 200) {
     throw new Error("Unable to delete chat");
   }
@@ -59,7 +67,7 @@ export const deleteUserChats = async () => {
 };
 
 export const logoutUser = async () => {
-  const res = await axios.get("/user/logout");
+  const res = await axios.get("/user/logout", { withCredentials: true });
   if (res.status !== 200) {
     throw new Error("Logout failed");
   }
